9e6b8d9707fcdf921d3adb17643afebe38e658c4
Anshit Chauhan <chauhananshit48@gmail.com>
2025-09-27 14:23:40 +0530

    add joi for schema validation

--- backend/app/routes/companyRoutes.js
+++ backend/app/routes/companyRoutes.js
@@ -1,7 +1,9 @@
 const companyController = require('../controller/companyController');
+const { schemaValidation } = require('../middleware/schemaValidation');
+const joiSchema = require('../services/schemaService');
 
 const companyRoutes = require('express').Router();
 
-companyRoutes.post('/add-company', companyController.addCompany);
+companyRoutes.post('/add-company', schemaValidation(joiSchema.addCompanySchema), companyController.addCompany);
 
 module.exports = companyRoutes;
---
+++ backend/app/services/schemaService.js
@@ -0,0 +1,16 @@
+const joi = require('joi');
+
+const joiSchema = {};
+
+joiSchema.addCompanySchema = {
+    body: joi.object({
+        name: joi.string().required(),
+        industry: joi.string().required(),
+        location: joi.string().required(),
+        founded: joi.number().required(),
+        employeeCount: joi.number().required(),
+        revenue: joi.number().required(),
+    })
+}
+
+module.exports = joiSchema;
